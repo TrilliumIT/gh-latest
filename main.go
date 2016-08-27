69341a686799f1b41cf78295dfbe7f7da3fcfce9
Travis Hegner <travis.hegner@gmail.com>
2016-08-27 16:14:44 -0400

    working with files and archives

--- main.go
+++ main.go
@@ -14,21 +14,57 @@ func main() {
 }
 
 func handleRepo(w http.ResponseWriter, r *http.Request) {
+	reqLocationSlice := strings.Split(r.URL.Path, "/")
+	user := reqLocationSlice[2]
+	project := reqLocationSlice[3]
+	file := reqLocationSlice[4]
+
+	tag, err := getTag(user, project)
+	if err != nil {
+		http.Error(w, err.Error(), 502)
+	}
+
+	fileUrl := ""
+	if file == "archive.tar.gz" || file == "archive.zip" {
+		parts := strings.Split(file, ".")
+		atype := "tar.gz"
+		if len(parts) == 2 {
+			atype = "zip"
+		}
+		fileUrl = fmt.Sprintf("https://github.com/%v/%v/archive/%v.%v", user, project, tag, atype)
+	}
+
+	if fileUrl == "" {
+		fileUrl = fmt.Sprintf("https://github.com/%v/%v/releases/download/%v/%v", user, project, tag, file)
+	}
+
+	http.Redirect(w, r, fileUrl, 302)
+}
+
+func handleRoot(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>gh-latest</h1><p>A simple project to get specific files from the latest release of any github repo.</p><p>Use a url like /repo/$user/$project/$filename to download that file from a github release.</p>")
+}
+
+func getTag(user, project string) (string, error) {
 	client := &http.Client{
 		CheckRedirect: func(req *http.Request, via []*http.Request) error {
 			return http.ErrUseLastResponse
 		},
 	}
-	loc := strings.Split(r.URL.Path, "/")
-	url := fmt.Sprintf("https://github.com/%v/%v/releases/latest", loc[2], loc[3])
+	url := fmt.Sprintf("https://github.com/%v/%v/releases/latest", user, project)
 	fmt.Println(url)
 	resp, err := client.Head(url)
+	defer resp.Body.Close()
 	if err != nil {
-		panic(err)
+		return "", fmt.Errorf("Error retrieving latest location from github.com")
 	}
-	fmt.Printf("%+v", resp)
-}
 
-func handleRoot(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>gh-latest</h1><p>A simple project to get specific files from the latest release of any github repo.</p><p>Use a url like /repo/$user/$project/$filename to download that file from a github release.</p>")
+	if resp.StatusCode != 302 {
+		return "", fmt.Errorf("Unexpected status code from github.com")
+	}
+
+	redirLocation := resp.Header.Get(http.CanonicalHeaderKey("Location"))
+	redirLocationSlice := strings.Split(redirLocation, "/")
+
+	return redirLocationSlice[len(redirLocationSlice)-1], nil
 }
